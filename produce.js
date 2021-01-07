8b06092c89c9f107601ac532ed797f491152dab5
John-ux13 <76175705+John-ux13@users.noreply.github.com>
2021-01-07 09:55:25 -0500

    Add files via upload

---
+++ produce.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Produce</h2>
+
+<p></p>
+
+<script>
+var Name = "produce";
+
+var Colors = ["white", "Black", "red"];
+
+var produce = "White";  // String written inside quotes
+var produce = "black";  // String written inside quotes
+var produce = "red";  // String written inside quotes
+document.getElementById("display").innerHTML = produce;
+
+</script>
+
+</body>
+</html>
+
+
+
+
+
